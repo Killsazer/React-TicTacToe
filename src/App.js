d158f543567c1f71e2b4b9c66b32ba95c8f15b17
killsazer <vadym2099@gmail.com>
2025-03-27 23:16:52 +0200

    Added win condition and status
    - Created win condition
    - Created new function -> Game
    - Created status, that show whose turn is next
    - Worked on css file to center status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { useState } from 'react';
 
-export default function Board() {
+export default function Game()
+{
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState(Array(9).fill(null));
+  const currentSquares = history[history.length - 1];
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board/>
+      </div>
+    </div>
+  );
+}
+
+function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i){
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
     //можна зробити й без створення нового масиву, але так краще (immutability
@@ -21,33 +36,59 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner: " + winner;
+  }
+  else{
+    status = "Next player: " + (xIsNext ? "X" : "O")
+  }
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for(let i = 0; i < lines.length; i++){
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <>
-      <div className='game'>
-        <div className='board'>
-          <div className="board-row">
-            <Square value = {squares[0]} onSquareCLick = {() => handleClick(0)}/>
-            <Square value = {squares[1]} onSquareCLick = {() => handleClick(1)}/>
-            <Square value = {squares[2]} onSquareCLick = {() => handleClick(2)}/>  
-          </div>
+      <div className='status'>{status}</div>
 
-          <div className="board-row">
-            <Square value = {squares[3]} onSquareCLick = {() => handleClick(3)}/>  
-            <Square value = {squares[4]} onSquareCLick = {() => handleClick(4)}/>  
-            <Square value = {squares[5]} onSquareCLick = {() => handleClick(5)}/>  
-          </div>
-
-          <div className="board-row">
-            <Square value = {squares[6]} onSquareCLick = {() => handleClick(6)}/>  
-            <Square value = {squares[7]} onSquareCLick = {() => handleClick(7)}/>  
-            <Square value = {squares[8]} onSquareCLick = {() => handleClick(8)}/>
-          </div>
-        </div>
-
-        <div>
-          <button className='reset' onClick={() => setSquares(Array(9).fill(null))}>Reset</button>
-        </div>
+      <div className="board-row">
+        <Square value = {squares[0]} onSquareCLick = {() => handleClick(0)}/>
+        <Square value = {squares[1]} onSquareCLick = {() => handleClick(1)}/>
+        <Square value = {squares[2]} onSquareCLick = {() => handleClick(2)}/>  
       </div>
+
+      <div className="board-row">
+        <Square value = {squares[3]} onSquareCLick = {() => handleClick(3)}/>  
+        <Square value = {squares[4]} onSquareCLick = {() => handleClick(4)}/>  
+        <Square value = {squares[5]} onSquareCLick = {() => handleClick(5)}/>  
+      </div>
+
+      <div className="board-row">
+        <Square value = {squares[6]} onSquareCLick = {() => handleClick(6)}/>  
+        <Square value = {squares[7]} onSquareCLick = {() => handleClick(7)}/>  
+        <Square value = {squares[8]} onSquareCLick = {() => handleClick(8)}/>
+      </div>
+
+      <button className='reset' onClick={() => setSquares(Array(9).fill(null))}>Reset</button>
     </>
   );
 }
