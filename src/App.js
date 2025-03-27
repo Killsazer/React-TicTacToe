434da6c61a5f29448231f25c3494a3a4e3f802ff
killsazer <vadym2099@gmail.com>
2025-03-27 02:23:43 +0200

    Board and Square functions logic has been changed.
    - Now we are changed our square from emptiness to X in parent Bord function, so then we can create a win condition
    -also we learned about and used immutability

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import { useState } from 'react';
 
 export default function Board() {
-  const [click, setClick] = useState(0);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    //можна зробити й без створення нового масиву, але так краще (immutability
+    //наприклад задля історії ходів
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square click={click} setClick={setClick} />
-        <Square click={click} setClick={setClick} />
-        <Square click={click} setClick={setClick} />  
+        <Square value = {squares[0]} onSquareCLick = {() => handleClick(0)}/>
+        <Square value = {squares[1]} onSquareCLick = {() => handleClick(1)}/>
+        <Square value = {squares[2]} onSquareCLick = {() => handleClick(2)}/>  
       </div>
       <div className="board-row">
-        <Square click={click} setClick={setClick} />  
-        <Square click={click} setClick={setClick} />  
-        <Square click={click} setClick={setClick} />  
+        <Square value = {squares[3]} onSquareCLick = {() => handleClick(3)}/>  
+        <Square value = {squares[4]} onSquareCLick = {() => handleClick(4)}/>  
+        <Square value = {squares[5]} onSquareCLick = {() => handleClick(5)}/>  
       </div>
       <div className="board-row">
-        <Square click={click} setClick={setClick} />  
-        <Square click={click} setClick={setClick} />  
-        <Square click={click} setClick={setClick} />
+        <Square value = {squares[6]} onSquareCLick = {() => handleClick(6)}/>  
+        <Square value = {squares[7]} onSquareCLick = {() => handleClick(7)}/>  
+        <Square value = {squares[8]} onSquareCLick = {() => handleClick(8)}/>
       </div>
     </>
   );
 }
 
-function Square({click, setClick}) {
-  const [value, setValue] = useState(null);
-
-  function handleCLick() {
-    if(click % 2 === 0) {
-      setValue('X');
-    }
-    else
-    {
-      setValue('O');
-    }
-    setClick(click + 1);
-  }
-  return <button className="square" onClick = {handleCLick}>{value}</button>
+function Square({ value, onSquareCLick }) {
+  return <button className="square" onClick={onSquareCLick}>{value}</button>
 }
