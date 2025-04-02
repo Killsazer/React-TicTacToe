b5eea4ec63d72970c0dead7b71937792fe6a580d
killsazer <vadym2099@gmail.com>
2025-04-02 04:15:16 +0300

    Game modified a lot
    - Done a lot of work with front end
    *new colors
    * 'status' looks better and has animations now
    * added animations when you hover over the button and hover over the squares
    * added effect when someone win the game (animation + color)
    - Added status 'Draw'
    - Reset button has returned. Probably in the future it'll looks like 'yes' button and if you click on it -> you start new game

--- src/App.js
+++ src/App.js
@@ -1,49 +1,35 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 
-export default function Game()
-{
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState(Array(9).fill(null));
-  const currentSquares = history[history.length - 1];
-
-  return (
-    <div className='game'>
-      <div className='game-board'>
-        <Board/>
-      </div>
-    </div>
-  );
-}
-
-function Board() {
+export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const [resetTrigger, setResetTrigger] = useState(0);
 
   function handleClick(i){
     if(squares[i] || calculateWinner(squares)){
       return;
     }
-    //можна зробити й без створення нового масиву, але так краще (immutability
-    //наприклад задля історії ходів
     const nextSquares = squares.slice();
-    if(xIsNext){
-      nextSquares[i] = 'X';
-    }
-    else{
-      nextSquares[i] = 'O';
-    }
+    nextSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
 
-  const winner = calculateWinner(squares);
-  let status;
-  if(winner){
-    status = "Winner: " + winner;
-  }
-  else{
-    status = "Next player: " + (xIsNext ? "X" : "O")
-  }
+  const winner = calculateWinner(squares)?.win;
+  const winningLine = calculateWinner(squares)?.line || [];
+  const isDraw = calculateWinner(squares);
+
+  const [status, setStatus] = useState('');
+  useEffect(() => {
+    if(isDraw === 1){
+      setResetTrigger(prev => prev + 1);
+      setStatus(() => "Нічия!")
+    }
+    else{
+      setStatus (() => winner ? "Переможець: " + winner : "Зараз ходять: " + (xIsNext ? "X" : "O") ); 
+    }
+  }, [winner, xIsNext, isDraw, setStatus]);
 
   function calculateWinner(squares){
     const lines = [
@@ -60,39 +46,58 @@ function Board() {
     for(let i = 0; i < lines.length; i++){
       const [a, b, c] = lines[i];
       if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-        return squares[a];
+        return {
+          win: squares[a],
+          line: [a, b, c]
+        };
       }
     }
+
+    if(squares.every(square => square !== null)){
+      return 1;
+    }
+
     return null;
+  }
+  function reset(){
+    setSquares(Array(9).fill(null));
+    setResetTrigger(prev => prev + 1);
   }
 
   return (
     <>
-      <div className='status'>{status}</div>
+      <div className='game'>
+        <div className={`status ${winner ? 'status--win': ''}`} key={`-${resetTrigger}`}>
+          {status}
+        </div>
 
-      <div className="board-row">
-        <Square value = {squares[0]} onSquareCLick = {() => handleClick(0)}/>
-        <Square value = {squares[1]} onSquareCLick = {() => handleClick(1)}/>
-        <Square value = {squares[2]} onSquareCLick = {() => handleClick(2)}/>  
+        <div className='board-row'>
+          <Square value = {squares[0]} onSquareClick = {() => handleClick(0)} isWining={winningLine.includes(0)}/>
+          <Square value = {squares[1]} onSquareClick = {() => handleClick(1)} isWining={winningLine.includes(1)}/>
+          <Square value = {squares[2]} onSquareClick = {() => handleClick(2)} isWining={winningLine.includes(2)}/>  
+        </div>
+
+        <div className='board-row'>
+          <Square value = {squares[3]} onSquareClick = {() => handleClick(3)} isWining={winningLine.includes(3)}/>  
+          <Square value = {squares[4]} onSquareClick = {() => handleClick(4)} isWining={winningLine.includes(4)}/>  
+          <Square value = {squares[5]} onSquareClick = {() => handleClick(5)} isWining={winningLine.includes(5)}/>  
+        </div>
+
+        <div className='board-row'>
+          <Square value = {squares[6]} onSquareClick = {() => handleClick(6)} isWining={winningLine.includes(6)}/>  
+          <Square value = {squares[7]} onSquareClick = {() => handleClick(7)} isWining={winningLine.includes(7)}/>  
+          <Square value = {squares[8]} onSquareClick = {() => handleClick(8)} isWining={winningLine.includes(8)}/>
+        </div>
+
+        <button className='reset' onClick={reset}>
+          Reset
+        </button>
+
       </div>
-
-      <div className="board-row">
-        <Square value = {squares[3]} onSquareCLick = {() => handleClick(3)}/>  
-        <Square value = {squares[4]} onSquareCLick = {() => handleClick(4)}/>  
-        <Square value = {squares[5]} onSquareCLick = {() => handleClick(5)}/>  
-      </div>
-
-      <div className="board-row">
-        <Square value = {squares[6]} onSquareCLick = {() => handleClick(6)}/>  
-        <Square value = {squares[7]} onSquareCLick = {() => handleClick(7)}/>  
-        <Square value = {squares[8]} onSquareCLick = {() => handleClick(8)}/>
-      </div>
-
-      <button className='reset' onClick={() => setSquares(Array(9).fill(null))}>Reset</button>
     </>
   );
 }
 
-function Square({ value, onSquareCLick }) {
-  return <button className="square" onClick={onSquareCLick}>{value}</button>
+function Square({ value, onSquareClick, isWining}) {
+  return <button className={`square ${isWining ? 'square--win' : ''}`} onClick={onSquareClick}>{value}</button>
 }
